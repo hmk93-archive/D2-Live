d8b28a5d9c509a63cef6f756c7277c31565f9ee8
hmk <77431375+hmk93-archive@users.noreply.github.com>
2025-08-26 22:53:24 +0900

    GA_Heal 추가

--- D2/Source/LyraGame/AbilitySystem/LyraTaggedActor.h
+++ D2/Source/LyraGame/AbilitySystem/LyraTaggedActor.h
@@ -29,8 +29,11 @@ public:
 	//~End of UObject interface
 
 protected:
+
+	// @D2 Start - B_Hero_Default 에서 Outfits 설정 시 성별 Tag 를 추가할 수 있도록 BlueprintReadOnly 를 BlueprintReadWrite 로 변경
 	// Gameplay-related tags associated with this actor
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Actor)
 	FGameplayTagContainer StaticGameplayTags;
+	// @D2 End
 };
 
