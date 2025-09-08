15ebaac79390a0043e41b2330d259c51e1f132c4
ipad <77431375+hmk93-archive@users.noreply.github.com>
2025-09-08 17:18:25 +0900

    Hair Groom Attach Component to Component

--- D2/Source/LyraGame/Inventory/InventoryFragment_EquippableItem.h
+++ D2/Source/LyraGame/Inventory/InventoryFragment_EquippableItem.h
@@ -18,4 +18,9 @@ class UInventoryFragment_EquippableItem : public ULyraInventoryItemFragment
 public:
 	UPROPERTY(EditAnywhere, Category=Lyra)
 	TSubclassOf<ULyraEquipmentDefinition> EquipmentDefinition;
+
+	// @D2 Start - 장착아이템을 Widget에 표시하기 위한 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="D2|Equip")
+	FName ItemName;
+	// @D2 End
 };
